443ce48797424c0836c21ae0bc55bdce0fb13f50
Pedro Lima <pedrolima.ep@gmail.com>
2025-09-15 08:34:03 +0000

    Fixed dereferencing of null pointer.

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -14,13 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t				i;
+    const unsigned char	*ptr;
 
+    ptr = (const unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return (&((void *)s)[i]);
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)(ptr + i));
 		i++;
 	}
 	return (NULL);
